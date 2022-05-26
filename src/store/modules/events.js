a06de851f07d01c62eb49215604259a97e1b277d
soccerjesse <jishperu10@gmail.com>
2022-05-26 23:55:41 +0900

    reserve function

--- src/store/modules/events.js
+++ src/store/modules/events.js
@@ -1,30 +1,25 @@
 import axios from "axios";
+import { serializeEvent } from '../../functions/serializers';
 
 const apiUrl = "http://localhost:3000";
 
 const state = {
   events: [],
   event: null,
+  isEditMode: false,
 };
 
 const getters = {
-  events: state => state.events.map(event => {
-    return {
-      ...event,
-      start: new Date(event.start),
-      end: new Date(event.end)
-    };
-  }),
-  event: state => state.event ? {
-    ...state.event,
-    start: new Date(state.event.start),
-    end: new Date(state.event.end)
-  } : null,
+  events: state => state.events.map(event => serializeEvent(event)),
+  event: state => serializeEvent(state.event),
+  isEditMode: state => state.isEditMode,
 };
 
 const mutations = {
   setEvents: (state, events) => (state.events = events),
+  appendEvent: (state, event) => (state.events = [...state.events, event]),
   setEvent: (state, event) => (state.event = event),
+  setEditMode: (state, bool) => (state.isEditMode = bool),
 };
 
 const actions = {
@@ -32,8 +27,15 @@ const actions = {
     const response = await axios.get(`${apiUrl}/events`);
     commit("setEvents", response.data);
   },
+  async createEvent({ commit }, event) {
+    const response = await axios.post(`${apiUrl}/events`, event);
+    commit('appendEvent', response.data);
+  },
   setEvent({ commit }, event) {
     commit('setEvent', event);
+  },
+  setEditMode({ commit }, bool) {
+    commit('setEditMode', bool)
   },
 };
 
